0a13d1c1558faa028803fa69b35696fb0ce61d9d
Riho Kirss <rix@a9-server.kirss.ee>
2026-09-12 17:37:33 +0300

    fix: inherit the primary scale for derived profile transforms
    (cherry picked from commit 08d3c1c51235cdd0b9f72713964af27e159d1cf2)

--- src/cpp/web-ifc/geometry/IfcGeometryLoader.cpp
+++ src/cpp/web-ifc/geometry/IfcGeometryLoader.cpp
@@ -3806,6 +3806,8 @@ namespace webifc::geometry
         scale1 = _loader.GetDoubleArgument();
       }
 
+      // Scl2 inherits Scl when the optional Scale2 is omitted.
+      scale2 = scale1;
       if (lineType == schema::IFCCARTESIANTRANSFORMATIONOPERATOR2DNONUNIFORM)
       {
         _loader.MoveToArgumentOffset(expressID, 4);
@@ -3814,11 +3816,6 @@ namespace webifc::geometry
           _loader.StepBack();
           scale2 = _loader.GetDoubleArgument();
         }
-      }
-
-      if (lineType == schema::IFCCARTESIANTRANSFORMATIONOPERATOR2D)
-      {
-        scale2 = scale1;
       }
 
       return glm::dmat3(
